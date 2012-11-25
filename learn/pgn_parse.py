eda19247d8e855c1ec731ed3f2425625665931a2
Naftali Harris <naftaliharris@gmail.com>
2012-11-24 22:13:25 -0800

    Added ChessGameFile class

--- learn/pgn_parse.py
+++ learn/pgn_parse.py
@@ -1,7 +1,10 @@
 # pgn_parse.py
 # tools for parsing pgns
 
+from __future__ import division
 import re
+import random
+import sys
 
 class ChessGame:
     def __init__(self, lines):
@@ -38,6 +41,50 @@ class ChessGame:
         result += "\nGAME OVER\n"
 
         return result
+
+class ChessGameFile:
+    def __init__(self, f):
+        self.f = f
+        self.num_games = 0
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        newlines = 0
+        lines = []
+        while newlines < 2:
+            line = self.f.readline()
+            if line.strip():
+                lines.append(line)
+            else:
+                newlines += 1
+        self.num_games += 1
+        if lines:
+            return ChessGame(lines)
+        else:
+            raise StopIteration
+
+    def random_games(self, n, use_game=lambda x: True):
+        games = []
+        while len(games) < n:
+            game = self.next()
+            if use_game(game):
+                games.append(game)
+        total = n
+        for game in self:
+            if use_game(game):
+                total += 1
+                if random.random() < n / total:
+                    games[random.randrange(n)] = game
+        random.shuffle(games)
+        return games
+
+def main():
+    with open(sys.argv[1], "r") as f:
+        cgf = ChessGameFile(f)
+        for game in cgf.random_games(5):
+            print game
 
 if __name__ == "__main__":
     main()
